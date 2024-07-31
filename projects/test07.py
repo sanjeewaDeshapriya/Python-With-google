d11ec08bcdc1265604cd47f5aa1a409e9727f875
Dilshan Sanjeewa Deshapriya <dilshansanjeewadeshappriya@gmail.com>
2024-07-31 11:53:06 +0530

    update

--- projects/test07.py
+++ projects/test07.py
@@ -10,3 +10,5 @@ def count_users(group):
 print(count_users("sales")) # Should be 3
 print(count_users("engineering")) # Should be 8
 print(count_users("everyone")) # Should be 18
+#need to update all the codes now 
+#as well as need to implement the get _ members methord 
