a2c84bb0a39e69bd3736340a1e5b533728a35c7a
Dilshan Sanjeewa Deshapriya <dilshansanjeewadeshappriya@gmail.com>
2024-08-03 08:11:32 +0530

    mak a file for string

---
+++ projects/test09.py
@@ -0,0 +1,46 @@
+name = "Sanjeewa deshapriya"
+
+#String slicing
+
+print(name)
+print(name[0:4])
+print(name[:4])
+print(name[4:])
+print(name[-1:5])
+
+#string indexing
+
+print(name.index("e"))
+print(name.index("jeewa"))
+print(name.index("hap"))
+
+#string item count
+
+print(name.count("e"))
+print(name.count("esh"))
+
+#is in in string 
+
+print("San" in name)
+print("Sanjeewa" in name)
+
+animals = "lions tigers and bears"
+print("tigers" in animals)
+
+#variabale case
+print(name.upper())
+print(name.lower())
+
+#strip remove spaces and the make shorter and clean 
+print(" yes")
+print(" yes".strip())
+verb =" cat     "
+print(verb.lstrip())
+print(verb.rstrip())
+
+#split and joiun the strings 
+
+name="Hi my name is sanjeewa deshappriya"
+namesplit=name.split()
+print(namesplit)
+print("_".join(namesplit))
